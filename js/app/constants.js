c9a1e41dc2fcbfba8821e49caa02aac3b7650c9d
web-intelligence <info@webintelligence.ie>
2014-02-20 15:05:07 +0000

    third commit

--- js/app/constants.js
+++ js/app/constants.js
@@ -5,7 +5,7 @@
 
     var project_title = "mallow", 
         project_number = "929254445509", 
-        server_url = "http://schoolspace.me", 
+        server_url = "http://push.schoolspace.ie", 
         standard_device_id = "63843", 
         standard_api_key = "hv7Vgd4jsbb",
         twitter_handle = "MallowCollege1";  
